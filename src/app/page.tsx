1e99c2588601ea6d1da0e9acd51ca8937a35cf15
atemengue <moafembe@gmail.com>
2024-02-03 02:50:16 +0100

    add reusable component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import Link from 'next/link';
+import Hero from '@/components/hero';
+import homeImg from '/public/home.jpg';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Hero
+      imgAlt="car factory"
+      title="Professionnal Cloud Hosting"
+      imgData={homeImg}
+    />
+  );
 }
