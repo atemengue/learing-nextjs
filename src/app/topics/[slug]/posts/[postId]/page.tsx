1ff053a017a8d954f5a0c47dcb611ccc34358d4c
atemengue <moafembe@gmail.com>
2024-02-08 04:38:45 +0100

    show post page

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,21 @@
-export default function PostShowPage() {
-  return <div>Post Show Page</div>
+import PostShow from '@/components/posts/post-show';
+import { paths } from '@/paths';
+import Link from 'next/link';
+
+interface PostShowPageProps {
+  params: {
+    slug: string,
+    postId: string
+  }
+}
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
+  return <div className="space-y-3">
+    <Link className="underline decoration-solid" href={paths.topciShowPath(slug)}>
+      {'< '} Back to { slug }
+    </Link>
+    <PostShow postId={postId} />
+
+  </div>
 }
--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -7,11 +7,12 @@ export type PostWithData = (
     user: { name: string | null };
     _count: { comments: number}
   }
-)
+);
+
+// export type PostWithData = Awaited<ReturnType<typeof fetchPostsByTopicSlug>>[number];
 
 export function fetchPostsByTopicSlug(slug: string):
-Promise<PostWithData[]>
-{
+Promise<PostWithData[]>{
   return db.post.findMany({
     where: { topic: { slug }},
     include: {
